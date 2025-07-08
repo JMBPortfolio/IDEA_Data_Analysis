d5802989e351ed934f66db776aa32e2356ba8849
JMBPortfolio <johnmbrenner@johnmbrenner.com>
2025-07-08 16:11:53 -0500

    Update Idea_Graphs.py

--- Idea_Graphs.py
+++ Idea_Graphs.py
@@ -8,16 +8,22 @@ df = pd.read_csv(filename)
 df["Grant Total"] = df["Grant Total"].astype(float)
 
 #Group and sort in pandas
+df = df[df["State"] != "US Total" ]
 state_totals = df.groupby("State")["Grant Total"].sum().sort_values()
 
 #Plot
 import matplotlib.pyplot as plt
 plt.style.use("ggplot")
-state_totals.nlargest(15).plot(kind="bar", figsize=(5,6))
+state_totals.nlargest(25).plot(kind="bar", figsize=(5,6))
 plt.xlabel("Total Grant ($)")
 plt.title("IDEA Funding by State")
 plt.tight_layout()
 
-#output and display
-plt.savefig("State_totals.svg")
+#output as {filename}_plot.svg
+import os
+
+stem = os.path.splitext(filename)[0]
+plt.savefig(f"{stem}_plot.svg")
+
+#Display
 plt.show()
