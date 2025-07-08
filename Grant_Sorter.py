eb8ca271a35bb9f3f5b89e6238798609708fb810
JMBPortfolio <johnmbrenner@johnmbrenner.com>
2025-07-08 16:14:16 -0500

    Update Grant_Sorter.py

--- Grant_Sorter.py
+++ Grant_Sorter.py
@@ -1,6 +1,6 @@
 import csv
 
-# Get user input
+# Get user input and  add .csv so you don't have to type it
 filename = input("Enter CSV filename (e.g., file1.csv): ").strip()
 if not filename.endswith(".csv"):
     filename += ".csv"
@@ -12,6 +12,7 @@ with open(filename, newline="") as f:
         state = row["State"]
         grant = float(row["Grant Total"].replace("$", "").replace(",", ""))
         states_grants.append((state, grant))
+        
 #store sorted list to be saved in new file
 sorted_data = sorted(states_grants, key=lambda p: p[1])
 
@@ -22,5 +23,6 @@ with open(new_filename, "w", newline="") as out:
     writer.writerow(["State", "Grant Total"])
     for state, grant in sorted_data:
         writer.writerow([state, f"{grant:.2f}"])
-
+        
+#Confirmation of new saved file
 print(f"Sorted data saved to {new_filename}")
