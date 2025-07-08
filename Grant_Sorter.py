85286b0a4dd229580793b122f0203e208055ffe2
John M Brenner <johnmbrenner@johnmbrenner.com>
2025-07-08 16:23:02 -0500

    Update Grant_Sorter.py

--- Grant_Sorter.py
+++ Grant_Sorter.py
@@ -1,6 +1,10 @@
+"""  
+Grant_Sorter.py  
+Reads a CSV of IDEA grant data, extracts state totals, and saves a sorted list.  
+"""  
 import csv
 
-# Get user input and  add .csv so you don't have to type it
+# get user input and  add .csv so you don't have to type it
 filename = input("Enter CSV filename (e.g., file1.csv): ").strip()
 if not filename.endswith(".csv"):
     filename += ".csv"
@@ -16,7 +20,7 @@ with open(filename, newline="") as f:
 #store sorted list to be saved in new file
 sorted_data = sorted(states_grants, key=lambda p: p[1])
 
-#Create the new file with "_Sorted"
+#create the new file with "_Sorted"
 new_filename = (filename[:-4] if filename.endswith(".csv") else filename) + "_Sorted.csv"
 with open(new_filename, "w", newline="") as out:
     writer = csv.writer(out)
@@ -24,5 +28,5 @@ with open(new_filename, "w", newline="") as out:
     for state, grant in sorted_data:
         writer.writerow([state, f"{grant:.2f}"])
         
-#Confirmation of new saved file
+#confirmation of new saved file
 print(f"Sorted data saved to {new_filename}")
