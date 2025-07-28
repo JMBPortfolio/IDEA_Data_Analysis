64005a7b94fddd1aebdc368299a6dfaf94207513
John M Brenner <johnmbrenner@johnmbrenner.com>
2025-07-28 17:10:30 -0500

    Update Idea_Graphs.py

--- Idea_Graphs.py
+++ Idea_Graphs.py
@@ -11,11 +11,11 @@ df = pd.read_csv(filename)
 #convert to float and strip the symbols again JIC (want to make sure it's able to convert to float)
 df["Grant Total"] = df["Grant Total"].astype(float)
 
-#Group and sort in pandas
+#group and sort in pandas
 df = df[df["State"] != "US Total" ]
 state_totals = df.groupby("State")["Grant Total"].sum().sort_values()
 
-#Plot
+#plot
 import matplotlib.pyplot as plt
 plt.style.use("ggplot")
 state_totals.nlargest(25).plot(kind="bar", figsize=(5,6))
@@ -29,5 +29,5 @@ import os
 stem = os.path.splitext(filename)[0]
 plt.savefig(f"{stem}_plot.svg")
 
-#Display
+#display
 plt.show()
